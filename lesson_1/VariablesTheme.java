1d2241af56ea44b4837b04e19f30079c9eff6e9e
Lekbear <Lek36820@gmail.com>
2023-03-12 22:50:18 +0300

    Исправление замечаний в VariablesTheme

--- lesson_1/VariablesTheme.java
+++ lesson_1/VariablesTheme.java
@@ -13,6 +13,8 @@ public class VariablesTheme {
     }
 
     private static void printPrimitiveVariables() {
+        System.out.println("1. Вывод значений переменных на консоль");
+
         byte ram = 16;
         short shaders = 24;
         int busWidth = 128;
@@ -22,7 +24,6 @@ public class VariablesTheme {
         char rop = '8';
         boolean openCl = true;
 
-        System.out.println("1. Вывод значений переменных на консоль");
         System.out.println("Количество гигабайт оперативной памяти: " + ram);
         System.out.println("Количество шейдерных процессов в видеоадаптере: " + shaders);
         System.out.println("Ширина шины между графическим процессором и видеопамятью: " + busWidth);
@@ -36,17 +37,18 @@ public class VariablesTheme {
     }
 
     private static void printCalcPrices() {
+        System.out.println("2. Расчет стоимости товара со скидкой");
+
         float penPrice = 100f;
         float bookPrice = 200f;
+        float sumPrice = penPrice + bookPrice;
         float discount = 11f;
-        float totalPriceNoDiscount = penPrice + bookPrice;
-        float sumDiscount = totalPriceNoDiscount * discount / 100f;
-        float totalPriceDiscount = totalPriceNoDiscount - sumDiscount;
+        float discountSum = sumPrice * discount / 100f;
+        float discountPrice = sumPrice - discountSum;
 
-        System.out.println("2. Расчет стоимости товара со скидкой");
-        System.out.println("Общая стоимость товаров без скидки: " + totalPriceNoDiscount);
-        System.out.println("Общая стоимость товаров со скидкой: " + sumDiscount);
-        System.out.println("Сумма скидки: " + totalPriceDiscount + '\n');
+        System.out.println("Общая стоимость товаров без скидки: " + sumPrice);
+        System.out.println("Общая стоимость товаров со скидкой: " + discountPrice);
+        System.out.println("Сумма скидки: " + discountSum + '\n');
     }
 
     private static void printJava() {
@@ -58,12 +60,13 @@ public class VariablesTheme {
     }
 
     private static void printMax() {
+        System.out.println("4. Вывод min и max значений целых числовых типов");
+
         byte numberByte = Byte.MAX_VALUE; 
         short numberShort = Short.MAX_VALUE;
         int numberInt = Integer.MAX_VALUE;
         long numberLong = Long.MAX_VALUE;
 
-        System.out.println("4. Вывод min и max значений целых числовых типов");
         System.out.println("Byte, max = " + numberByte);
         System.out.println("Short, max = " + numberShort);
         System.out.println("Int, max = " + numberInt);
@@ -81,10 +84,11 @@ public class VariablesTheme {
     }
 
     private static void printSwapVariables() {
+        System.out.println("5. Перестановка значений переменных");
+
         int number1 = 2;
         int number2 = 5;
 
-        System.out.println("5. Перестановка значений переменных");
         System.out.println("5.1. C помощью третьей переменной:");
         System.out.println("Исходные значения: " + number1 + ", " + number2);
         int temp = number1;
@@ -108,62 +112,49 @@ public class VariablesTheme {
     }
 
     private static void printCodesSymbols() {
-        char symbol1 = '#';
-        char symbol2 = '&';
-        char symbol3 = '@';
-        char symbol4 = '^';
-        char symbol5 = '_';
-
-        int code_Symbol1 = symbol1;
-        int code_Symbol2 = symbol2;
-        int code_Symbol3 = symbol3;
-        int code_Symbol4 = symbol4;
-        int code_Symbol5 = symbol5;
-
         System.out.println("6. Вывод символов и их кодов");
-        System.out.println("Код символа: " + code_Symbol1 + ", символ: " + symbol1);
-        System.out.println("Код символа: " + code_Symbol2 + ", символ: " + symbol2);
-        System.out.println("Код символа: " + code_Symbol3 + ", символ: " + symbol3);
-        System.out.println("Код символа: " + code_Symbol4 + ", символ: " + symbol4);
-        System.out.println("Код символа: " + code_Symbol5 + ", символ: " + symbol5 + '\n');
+
+        char sharp = '#';
+        char ampersand = '&';
+        char at = '@';
+        char caret = '^';
+        char underscore = '_';
+
+        System.out.println("Код символа: " + (int) sharp + ", символ: " + sharp);
+        System.out.println("Код символа: " + (int) ampersand + ", символ: " + ampersand);
+        System.out.println("Код символа: " + (int) at + ", символ: " + at);
+        System.out.println("Код символа: " + (int) caret + ", символ: " + caret);
+        System.out.println("Код символа: " + (int) underscore + ", символ: " + underscore + '\n');
     }
 
     private static void printDuke() {
-        String space6 = "      ";
-        String space4 = "    ";
-        String space3 = "   ";
-        String space2 = "  ";
-        String space1 = " ";
-
-        char slashRight   = '/';
-        char slashLeft    = '\\';
-        char underLine    = '_';
-        char bracketLeft  = '(';
-        char bracketRight = ')'; 
-
         System.out.println("7. Вывод в консоль ASCII-арт Дюка");
-        System.out.println(space4 + slashRight + slashLeft + space4 + '\n' + 
-                space3 + slashRight + space2 + slashLeft + space3 + '\n' + 
-                space2 + slashRight + underLine + bracketLeft + space1 + bracketRight + slashLeft + 
-                space2 + '\n' +  
-                space1 + slashRight + space6 + slashLeft + space1 + '\n' + 
-                slashRight + underLine + underLine + underLine + underLine +
-                slashRight + slashLeft + underLine + underLine + slashLeft + '\n');
+
+        char slash   = '/';
+        char backslash    = '\\';
+        char underscore    = '_';
+        char openParenthesis  = '(';
+        char closeParenthesis = ')'; 
+
+        System.out.println("    " + slash + backslash + "    ");
+        System.out.println("   " + slash + "  " + backslash + "   ");
+        System.out.println("  " + slash + underscore + openParenthesis + " " + closeParenthesis + 
+                backslash + "  ");
+        System.out.println(" " + slash + "      " + backslash + " ");
+        System.out.println("" + slash + underscore + underscore + underscore + underscore + slash + 
+                backslash + underscore + underscore + backslash + "\n");
     }
 
     private static void printSumMulNumber() {
-        int number = 123;
         System.out.println("8. Вывод количества сотен, десятков и единиц числа");
-        System.out.println("Число " + number + " содержит: ");
-        
-        int ones = number % 10;
-        number /= 10;
-        
-        int tens = number % 10;
-        number /= 10;
 
-        int hundreds = number % 10;
-        number /= 10;
+        int number = 123;
+
+        System.out.println("Число " + number + " содержит: ");
+
+        int ones = number % 10;
+        int tens = number / 10 % 10;
+        int hundreds = number / 100 % 10;
 
         System.out.println(hundreds + " сотен");
         System.out.println(tens + " десяток");
@@ -173,13 +164,11 @@ public class VariablesTheme {
     }
 
     private static void printTime() {
-        int seconds = 86399;
-        int hours = seconds / 3600;
-        seconds %= 3600;
-        int minutes = seconds / 60;
-        seconds %= 60;
-
         System.out.println("9. Вывод времени");
-        System.out.println(hours + ':' + minutes + ':' + seconds);
+
+        int seconds = 86399;
+
+        System.out.println("" + seconds / 3600 + ':' + seconds % 3600 / 60 + ':' + 
+                seconds % 3600 / 60 % 60);
     }
 }
