9a303f222e919c0494dcd76b85cfe00cc7a8af52
Lekbear <Lek36820@gmail.com>
2023-03-24 22:03:28 +0300

    Новое ДЗ Person.java

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person{
+    String gender = "Мужской";
+    String name = "Alexey";
+    int height = 177;
+    int weight = 77;
+    int age = 24;
+
+    private void move() {
+        System.out.println("движемся");
+    }
+
+    private void sit() {
+        System.out.println("сидим");
+    }
+
+    private void run() {
+        System.out.println("бежим");
+    }
+
+    private void speak() {
+        System.out.println("говорим");
+    }
+
+    private void learnJava() {
+        System.out.println("Учим Java");
+    }
+}
