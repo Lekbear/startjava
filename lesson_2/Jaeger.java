41c3d5a19d2e20bb4db4fc737eb2dd71c9d05bae
Lekbear <Lek36820@gmail.com>
2023-03-31 22:36:19 +0300

    добавление классов JaegerTest.java Jaeger.java

---
+++ lesson_2/Jaeger.java
@@ -0,0 +1,54 @@
+public class Jaeger {
+    private String modelName;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {}
+
+    public Jaeger(String modelName, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void printAttributes() {
+        System.out.println("Параметры робота " + modelName + ":");
+        System.out.println("Скорость: " + speed);
+        System.out.println("Сила: " + strength);
+        System.out.println("Броня: " + armor);
+    }
+}
---
+++ lesson_2/JaegerTest.java
@@ -0,0 +1,18 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Gipsy Danger");
+        jaeger1.setSpeed(7);
+        jaeger1.setStrength(8);
+        jaeger1.setArmor(6);
+        jaeger1.printAttributes();
+
+        Jaeger jaeger2 = new Jaeger("Coyote Tango", 5, 7, 4);
+        jaeger2.printAttributes();
+
+        System.out.print("Общая сумма параметров " + jaeger1.getModelName() + " : ");
+        System.out.println(jaeger1.getSpeed() + jaeger1.getStrength() + jaeger1.getArmor());
+        System.out.print("Общая сумма параметров " + jaeger2.getModelName() + " : ");
+        System.out.println(jaeger2.getSpeed() + jaeger2.getStrength() + jaeger2.getArmor());
+    }
+}
