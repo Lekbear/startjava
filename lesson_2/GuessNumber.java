0bb7d0b85d6e88cf43e5acd7c58b0f1232a27fd3
Lekbear <Lek36820@gmail.com>
2023-04-02 18:28:46 +0300

    Исправление замечаний в классах: GuessNumber, Player

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -1,46 +1,63 @@
+import java.util.Scanner;
+
 public class GuessNumber {
+    private final int left = 1;
+    private final int right = 101;
     private Player player1;
     private Player player2;
-    private int left;
-    private int right;
     private int hiddenNumber;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
-        left = 1;
-        right = 101;
         hiddenNumber = (int) (Math.random() * (right - left) + left);
         System.out.println("Компьютер загадал число");
     }
 
     public void start() {
         while (true) {
-            System.out.print("Попытка игрока " + player1.getName() + " угадать число: ");
-            player1.guessNumber(left, right);
-            if (checkWin(player1.getNumber())) {
-                System.out.println("Игрок " + player1.getName() + " угадал загаданное число");
+            guessNumber(player1);
+            if (compareNumbers(player1)) {
                 break;
-            }
+            }  
 
-            System.out.print("Попытка игрока " + player2.getName() + " угадать число: " );
-            player2.guessNumber(left, right);
-            if (checkWin(player2.getNumber())) {
-                System.out.println("Игрок " + player2.getName() + " угадал загаданное число");
+            guessNumber(player2);
+            if (compareNumbers(player2)) {
                 break;
             }
-            
         }
     }
 
-    private boolean checkWin(int playerNumber) {
-        if (playerNumber > hiddenNumber) {
-            System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
-        } else if (playerNumber < hiddenNumber) {
-            System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
-        } else {
+    private boolean compareNumbers(Player player) {
+        if (player.getNumber() == hiddenNumber) {
+            System.out.println("Игрок " + player.getName() + " угадал загаданное число");
             return true;
         }
+
+        if (player.getNumber() > hiddenNumber) {
+            System.out.println("Число " + player.getNumber() + " больше того, что загадал компьютер");
+        } else {
+            System.out.println("Число " + player.getNumber() + " меньше того, что загадал компьютер");
+        } 
         return false;
+    }
+
+    public void guessNumber(Player player) {
+        System.out.print("Попытка игрока " + player.getName() + " угадать число: " );
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                int number = Integer.parseInt(scanner.nextLine());
+                if (number >= left && number <= right - 1) {
+                    player.setNumber(number);
+                    break;
+                }
+                System.out.print("Число нужно вводить в диапазоне (" + (left - 1) + ", " + 
+                        (right - 1) + "]: ");
+            } catch (NumberFormatException e) {
+                System.out.print("Неверный ввод!\nНужно ввести число: ");
+            }
+        }
     }
 }
--- lesson_2/Player.java
+++ lesson_2/Player.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Player {
     private String name;
     private int number;
@@ -16,19 +14,7 @@ public class Player {
         return number;
     }
 
-    public void guessNumber(int left, int right) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                number = Integer.parseInt(scanner.nextLine());
-                if (number >= left && number <= right - 1) {
-                    break;
-                }
-                System.out.print("Число нужно вводить в диапазоне (" + (left - 1) + ", " + 
-                        (right - 1) + "]: ");
-            } catch (NumberFormatException e) {
-                System.out.print("Неверный ввод!\nНужно ввести число: ");
-            }
-        }
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
