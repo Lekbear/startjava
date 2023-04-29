359bbd845d5e28c26960b12465da77db5ddb1837
Lekbear <Lek36820@gmail.com>
2023-04-29 20:32:55 +0300

    Добавление задания на тему "Базы данных"

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,11 +1,11 @@
 INSERT INTO jaegers (modelName, mark, height, weight, status, origin, launch, kaijuKill)
-VALUES ('Striker Eureka', 'Mark-5', '250', '1850', 'Destroyed', 'Australia', '2019', '11'),
-       ('Crimson Typhoon', 'Mark-4', '250', '1722 ', 'Destroyed', 'China', '2018', '7'),
-       ('Gipsy Danger', 'Mark-3', '260', '1980 ', 'Destroyed', 'USA', '2017', '9'),
-       ('Cherno Alpha', 'Mark-1', '280', '2412 ', 'Destroyed', 'Russia', '2015', '6'),
-       ('Tacit Ronin', 'Mark-1', '244', '7450 ', 'Destroyed', 'Japan', '2015', '3'),
-       ('Romeo Blue', 'Mark-1', '255', '7775 ', 'Destroyed', 'USA', '2015', '2'),
-       ('Horizon Brave', 'Mark-1', '238', '7890 ', 'Destroyed', 'China', '2015', '2'),
-       ('Coyote Tango', 'Mark-1', '280', '2312 ', 'Destroyed', 'Japan', '2015', '2'),
-       ('Brawler Yukon', 'Mark-1', '270', '2222 ', 'Destroyed', 'USA', '2015', '2'),
-       ('Gipsy Avenger', 'Mark-6', '268', '2004 ', 'Destroyed', 'USA', '2034', '1');
+VALUES ('Striker Eureka',  'Mark-5', '250', '1850', 'Destroyed', 'Australia', '2019', '11'),
+       ('Crimson Typhoon', 'Mark-4', '250', '1722', 'Destroyed', 'China',     '2018', '7'),
+       ('Gipsy Danger',    'Mark-3', '260', '1980', 'Destroyed', 'USA',       '2017', '9'),
+       ('Cherno Alpha',    'Mark-1', '280', '2412', 'Destroyed', 'Russia',    '2015', '6'),
+       ('Tacit Ronin',     'Mark-1', '244', '7450', 'Destroyed', 'Japan',     '2015', '3'),
+       ('Romeo Blue',      'Mark-1', '255', '7775', 'Destroyed', 'USA',       '2015', '2'),
+       ('Horizon Brave',   'Mark-1', '238', '7890', 'Destroyed', 'China',     '2015', '2'),
+       ('Coyote Tango',    'Mark-1', '280', '2312', 'Destroyed', 'Japan',     '2015', '2'),
+       ('Brawler Yukon',   'Mark-1', '270', '2222', 'Destroyed', 'USA',       '2015', '2'),
+       ('Gipsy Avenger',   'Mark-6', '268', '2004', 'Destroyed', 'USA',       '2034', '1');
